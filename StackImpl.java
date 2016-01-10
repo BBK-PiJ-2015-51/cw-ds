3d0cb1475fd8d925de2a73f96d31f9de97488b18
sssteven <sssteven@hotmail.co.uk>
2016-01-10 23:49:11 +0000

    comments

--- StackImpl.java
+++ StackImpl.java
@@ -1,37 +1,67 @@
+/** An implemantation of Stack that uses a {@List} that is stored in {@AbstractStack}
+*
+* @author SJENKI05
+*/
 public class StackImpl extends AbstractStack {
 	
+	/**
+	 * Constructor that sets parent class with list in parameter
+	 * @param the list to hold as stack 
+	 */
 	public StackImpl(List list) {
 		super(list);
 	}
 
+	/**
+	 * returns true if the stack is empty
+	 * @return true if the stack is empty
+	 */
 	public boolean isEmpty() {
 		return internalList.isEmpty();
 	}
 
+	/**
+	 * returns the size of the stack
+	 * @return he size of the stack
+	 */
 	public int size() {
 		return internalList.size();
 	}
 
+	/**
+	 * pushes a new object on the stack
+	 * @param object to be pushed on stack
+	 */
 	public void push(Object item) {
-
 		internalList.add(item);
 	}
 
+	/**
+	 * returns the top of the stack
+	 * @return return object at top of stack
+	 */
 	public ReturnObject top() {
-
-		if (internalList.get(size()-1) == null) {
+		int lastIndex = size()-1;
+		if (internalList.isEmpty()) {
 			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
 		} else {
-			return internalList.get(size()-1);
+			return internalList.get(lastIndex);
 		}
-		
 	}
 
+	/**
+	 * returns and removes the top of the stack
+	 * @return return object at top of stack
+	 */
 	public ReturnObject pop() {
-
-		ReturnObject temp = internalList.get(size()-1);
-		internalList.remove(size()-1);
+		int lastIndex = size()-1;
+		if (internalList.isEmpty()) {
+			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		} else {
+		ReturnObject temp = internalList.get(lastIndex);
+		internalList.remove(lastIndex);
 		return temp;
+		}
 	}
 
 }
