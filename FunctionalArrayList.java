e2d7e81af5405c66097c5e1ae94cf7067fece51b
sssteven <sssteven@hotmail.co.uk>
2016-01-10 23:46:16 +0000

    comments

--- FunctionalArrayList.java
+++ FunctionalArrayList.java
@@ -1,15 +1,31 @@
+/**
+ * An implementation of a functional list using {@arraylist}
+ *
+ *  @author SJENKI05
+ */
 public class FunctionalArrayList extends ArrayList implements FunctionalList {
 
+	/**
+ 	* holds the new Functional list to be returned
+ 	*/
 	private FunctionalList newFunctionalList;
 
+	/**
+ 	* returns the return object at head of the list
+ 	* @return the return object at head of the list
+ 	*/
 	public ReturnObject head() {
-		if (size() == 0) {
+		if (isEmpty()) {
 			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
 			} else {
 			return new ReturnObjectImpl(get(0).getReturnValue());
 		}
 	}
 
+	/**
+ 	* returns a new functional list excluding the head
+ 	* @return a new functional list excluding the head
+ 	*/
 	public FunctionalList rest() {
 		newFunctionalList = new FunctionalArrayList();
 		for(int i = 1; i < size(); i++) {
